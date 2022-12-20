6ca49ba22b030dc230cb88395dbd2737d32abf79
boudenjal-mohcine <mohcineboudenjal@gmail.com>
2022-12-20 12:09:04 +0100

    Updates

---
+++ precache-manifest.a42f11656ec23bbb37919d3d647644a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f7d7c8635c17e8d39ff",
+    "url": "/myportfolio/static/css/main.e79bfdf9.chunk.css"
+  },
+  {
+    "revision": "7f7d7c8635c17e8d39ff",
+    "url": "/myportfolio/static/js/main.805d104f.chunk.js"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.69c47bdd.js"
+  },
+  {
+    "revision": "8ebc307ffd537d123b52",
+    "url": "/myportfolio/static/js/2.c5820efb.chunk.js"
+  },
+  {
+    "revision": "6fcc9b21f18e0857209d67f96da5a720",
+    "url": "/myportfolio/index.html"
+  }
+];
