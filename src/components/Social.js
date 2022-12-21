f6dea3909e086b00716b4462b202ac9091abf194
boudenjal-mohcine <mohcineboudenjal@gmail.com>
2022-12-21 02:17:26 +0100

    add social media feature

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
+import { UsersIcon } from "@heroicons/react/solid";
+import { SocialIcon } from 'react-social-icons';
 
 
 export default function Social() {
@@ -10,6 +11,17 @@ export default function Social() {
       <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
         Social Media
       </h1>
+      <div className='flex justify-center space-x-5 md:space-x-8 sm:space-x-2'>
+        <SocialIcon url="https://twitter.com/boudenjalmohci1" />
+        <SocialIcon url="https://www.linkedin.com/in/mohcineboudenjal/" />
+        <SocialIcon url="https://api.whatsapp.com/send/?phone=0694464332" />
+        <SocialIcon url="https://github.com/boudenjal-mohcine" />
+        <SocialIcon url="https://www.facebook.com/boudnjalmohcine" />
+        <SocialIcon url="https://www.instagram.com/mohcinebd/" />
+        <SocialIcon url="https://discord.com/users/Mohcine#2607" />
+
+      </div>
+
     </div>
   </section>
   )
